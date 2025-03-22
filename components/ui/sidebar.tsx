eb3994fc73830508b0eaf45d44bd232a2c3fec95
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2025-03-22 22:39:18 +0530

    style: remove background class from chat header, form, and sidebar for a cleaner UI

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -324,7 +324,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex h-full flex-1 flex-col bg-background",
+        "relative flex h-full flex-1 flex-col",
         "peer-data-[variant=inset]:min-h-[calc(100%-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         "rounded-lg",
         className
